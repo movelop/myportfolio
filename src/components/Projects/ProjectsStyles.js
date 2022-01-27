9c37255b722f8615c5ee7884c49c7448746b5d67
Babalola Muhammad <barmilitary@yahoo.com>
2022-01-27 09:30:44 +0100

    project updated

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -73,6 +73,7 @@ export const CardInfo = styled.p`
   font-style: 2rem;
   line-height: 24px;
   text-align: justify;
+  margin-bottom : 20px;
   @media ${(props) => props.theme.breakpoints.sm} {
     padding:.3rem
   
